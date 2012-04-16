9339a5154507bf2f03439df170d4784bb2e867cb
Johnathan Botha <jokillsya@gmail.com>
2012-04-17 01:33:43 +0200

    Fixed object additions...

--- build/Debug/GNU-Linux-x86/src/lock_util.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux-x86/src/lock_util.o: src/lock_util.c \
- src/../include/plugz.h src/../include/lock_util.h
-
-src/../include/plugz.h:
-
-src/../include/lock_util.h:
---
+++ include/serv_zmq_layer.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   serv_zmq_layer.h
+ * Author: johnathan
+ *
+ * Created on 16 April 2012, 11:33 PM
+ */
+
+#ifndef SERV_ZMQ_LAYER_H
+#define	SERV_ZMQ_LAYER_H
+
+void *serv_init_zmq();
+
+#endif	/* SERV_ZMQ_LAYER_H */
+
